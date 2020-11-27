33763e0589a55c8650501aa6719f542c0bfe630d
WesleyVercoutere <wesley.vercoutere@gmail.com>
2020-11-27 06:20:11 +0100

    Update Callback.py

--- Callbacks/Callback.py
+++ Callbacks/Callback.py
@@ -10,10 +10,20 @@ class Callback:
         threading.Thread(target=self.runEvent, args=(time, callback)).start()
 
 
-    def runEvent(self, *args):
-        loopTime = args[0]
-        callback = args[1]
+    # def runEvent(self, *args):
+    #     loopTime = args[0]
+    #     callback = args[1]
 
+    #     startTime = time.time()
+
+    #     while True:
+    #         currentTime = time.time()
+
+    #         if currentTime >= startTime + loopTime:
+    #             startTime = time.time()
+    #             callback(loopTime)
+
+    def runEvent(self, loopTime, callback):
         startTime = time.time()
 
         while True:
