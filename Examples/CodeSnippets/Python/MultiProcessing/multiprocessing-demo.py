d30145565f6c15b52a340c09dd264bd6dda8032e
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-02-05 12:51:53 +0100

    Update multiprocessing-demo.py

--- Examples/CodeSnippets/Python/MultiProcessing/multiprocessing-demo.py
+++ Examples/CodeSnippets/Python/MultiProcessing/multiprocessing-demo.py
@@ -1,22 +1,29 @@
 import concurrent.futures
 import time
 
-start = time.perf_counter()
-
-
 def do_something(seconds):
-    print(f'Sleeping {seconds} second(s)...')
-    time.sleep(seconds)
-    return f'Done Sleeping...{seconds}'
+        print(f'Sleeping {seconds} second(s)...')
+        time.sleep(seconds)
+        return f'Done Sleeping...{seconds}'
+        
+def main():
+    print("start")
+    start = time.perf_counter()
 
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    secs = [5, 4, 3, 2, 1]
-    results = executor.map(do_something, secs)
+    
 
-    # for result in results:
-    #     print(result)
 
-finish = time.perf_counter()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        secs = [5, 4, 3, 2, 1]
+        results = executor.map(do_something, secs)
 
-print(f'Finished in {round(finish-start, 2)} second(s)')
+        # for result in results:
+        #     print(result)
+
+    finish = time.perf_counter()
+
+    print(f'Finished in {round(finish-start, 2)} second(s)')
+
+if __name__ == "__main__":
+    main()
