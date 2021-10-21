8d5ac90f2be99ae54ccdee9987a1d8175e741f02
WesleyVercoutere <wesley.vercoutere@gmail.com>
2021-10-21 13:30:33 +0200

    Update test_json.py

--- Json/test_json.py
+++ Json/test_json.py
@@ -9,11 +9,23 @@ class Person:
 
 
 
+
 if __name__ == "__main__":
 
     p1 = Person()
     p1.name = "Vercoutere"
     p1.firstname = "Wesley"
 
+    p2 = Person()
+    p2.name = "De Letter"
+    p2.firstname = "Vanessa"
+
     out = json.dumps(p1.__dict__)
     print(out)
+
+    persons = list()
+    persons.append(p1)
+    persons.append(p2)
+
+    out2 = json.dumps([p.__dict__ for p in persons])
+    print(out2)
